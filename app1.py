3ae79e98176d695958e7730f8d76e2c5be36d4d5
KeeganFernandesWork <62410110+KeeganFernandesWork@users.noreply.github.com>
2023-08-27 13:58:03 +0000

    version 1

--- app1.py
+++ app1.py
@@ -1,6 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer 
+import pickle
+import numpy as np
 
 app = Flask(__name__)
+
+# Text preprocessing functions
+def preprocess_text(text):
+    # Tokenization
+    words = word_tokenize(text.lower())  # Convert to lowercase and tokenize
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    
+    return ' '.join(words)
+
+
+# Load the vectorizer
+with open('model/classes.pkl', 'rb') as file:
+    classes = pickle.load(file)
+with open('model/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+with open('model/vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+
+st1 = '''I always thought Java was pass-by-reference; however I\'ve seen a couple of blog posts (for example, this blog) that claim it\'s not. I don\'t think I understand the distinction they\'re making. \n\nWhat is the explanation?\n Is Java "pass-by-reference" or "pass-by-value"?'''
+
+def get_tag(st1):
+    return classes[np.where(model.predict(vectorizer.transform([preprocess_text(st1)])).flatten() == 1)]
 
 # Sample data - replace this with your actual data
 questions = [
@@ -33,6 +68,7 @@ def ask():
             'id': len(questions) + 1,
             'title': request.form['title'],
             'content': request.form['content'],
+            "tags" : get_tag(request.form['content'] + request.form['title'])
         }
         questions.append(new_question)
         return redirect(url_for('index'))
