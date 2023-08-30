7d0b05a9cd52420cdb4b5136a6f89f084afc7780
Anujit Ghosh <anujit.ghosh@msds.christuniversity.in>
2023-08-30 16:10:54 +0000

    first deployment

--- app1.py
+++ app1.py
@@ -1,29 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer 
 import pickle
 import numpy as np
-from app2 import *
+from gradio_client import Client
 
+client = Client("https://keeganfdes-stack-onnx.hf.space/")
 
 app = Flask(__name__)
 
 # Text preprocessing functions
-def preprocess_text(text):
-    # Tokenization
-    words = word_tokenize(text.lower())  # Convert to lowercase and tokenize
-    
-    # Remove stopwords
-    stop_words = set(stopwords.words('english'))
-    words = [word for word in words if word not in stop_words]
-    
-    # Lemmatization
-    lemmatizer = WordNetLemmatizer()
-    words = [lemmatizer.lemmatize(word) for word in words]
-    
-    return ' '.join(words)
 
 
 # Load the vectorizer
@@ -37,7 +21,10 @@ with open('model/vectorizer.pkl', 'rb') as file:
 st1 = '''I always thought Java was pass-by-reference; however I\'ve seen a couple of blog posts (for example, this blog) that claim it\'s not. I don\'t think I understand the distinction they\'re making. \n\nWhat is the explanation?\n Is Java "pass-by-reference" or "pass-by-value"?'''
 
 def get_tag(st1):
-    return classes[np.where(model.predict(vectorizer.transform([preprocess_text(st1)])).flatten() == 1)]
+    asp = client.predict(st1,api_name="/predict")
+    input_string = asp.replace("'", "").replace("[", "").replace("]", "")
+    input_list = input_string.split()
+    return input_list
 
 # Sample data - replace this with your actual data
 questions = [
@@ -67,6 +54,7 @@ def index():
 def question(question_id):
     question = next((q for q in questions if q['id'] == question_id), None)
     if question:
+        print(question["tags"])
         return render_template('question1.html', question=question)
     return "Question not found"
 
@@ -80,8 +68,6 @@ def ask():
             'content': request.form['content'],
             "tags" : get_tag(request.form['content'] + request.form['title'])
         }
-        if request.form['butt'] == 1:
-            recognize_from_microphone()
         questions.append(new_question)
         return redirect(url_for('index'))
     return render_template('ask1.html')
